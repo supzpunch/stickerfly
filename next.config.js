b67c1b7bd32f49162d27ed910917be32cd18a302
SupScotty <supscotty@example.com>
2025-06-13 23:27:40 +0100

    Add Docker configuration for Coolify deployment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   reactStrictMode: true,
+  swcMinify: true,
   images: {
     domains: ['res.cloudinary.com'],
   },
